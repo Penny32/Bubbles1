b1f4290841786e67e68a280d2d6492887fa28b45
JasonLu1998 <jasonlu1998@yahoo.com>
2015-07-30 11:51:11 -0700

    JFrame

--- src/Bubbles.java
+++ src/Bubbles.java
@@ -1,7 +1,14 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+
 public class Bubbles extends JFrame {
+    private static final long serialVersionUID =1L;
     public static void main (String args[]) {
         new Bubbles().setVisible(true);
-        
+    }
+    private Main (){
+        super ("BUBBLES!");
+        setSize(600, 600); //1024x768 or 800x600
+        setResizable(false);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
