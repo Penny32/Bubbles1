69c3657947b70a81b0ad14bc943b4bed58653d5a
Penny Nguyen <i_throw_paper@yahoo.com>
2015-07-31 00:10:04 -0700

    Done with window

--- src/Bubbles.java
+++ src/Bubbles.java
@@ -1,14 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Bubbles extends JFrame {
     private static final long serialVersionUID =1L;
     public static void main (String args[]) {
         new Bubbles().setVisible(true);
     }
-    private Main (){
-        super ("BUBBLES!");
+    private Bubbles(){
+        super("BUBBLES!");
         setSize(600, 600); //1024x768 or 800x600
         setResizable(false);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+        JButton button = new JButton("Click Me");
+        add(button);
     }
 }
