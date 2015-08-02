ad0d67da39fa9759f9e87c91196c0efff5bb337d
Penny Nguyen <i_throw_paper@yahoo.com>
2015-08-02 14:43:22 -0700

    Added one circle

--- src/Bubbles.java
+++ src/Bubbles.java
@@ -1,25 +1,29 @@
-import com.sun.glass.ui.Size;
-import javafx.scene.shape.Circle;
-
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
+
+
 
 public class Bubbles extends JFrame {
-    private static final long serialVersionUID =1L;
+    private static final long serialVersionUID = 1L;
+
     public static void main(String args[]) {
         new Bubbles().setVisible(true);
-        Draw obeject = new Draw();
-        frame.add(object);
 
-        object.drawing();
     }
+
     private Bubbles() {
         super("BUBBLES!");
         setSize(600, 600); //1024x768 or 800x600
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new FlowLayout());
-        JButton button = new JButton("Click Me");
-        add(button);
+    }
+
+
+    public void paint(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.drawOval(200, 200, 50, 50);
     }
 }
+
