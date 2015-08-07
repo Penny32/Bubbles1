5c18754ff3dde559c74223ddb8cdaa106ec0eabd
Penny Nguyen <i_throw_paper@yahoo.com>
2015-08-06 21:34:31 -0700

    More progress but circles won't print anymore and I don't know how to fix it.

--- src/Bubbles.java
+++ src/Bubbles.java
@@ -1,39 +1,15 @@
 import javax.swing.*;
-import java.awt.*;
-import java.util.Random;
 
-
-public class Bubbles extends JFrame {
-    private static final long serialVersionUID = 1L;
-
+public class Bubbles {
     public static void main(String args[]) {
-        new Bubbles().setVisible(true);
-        }
-    private Bubbles() {
-        super("BUBBLES!");
-        setSize(600, 600); //1024x768 or 800x600
-        setResizable(false);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new FlowLayout());
+        draw s= new draw();
+        JFrame f = new JFrame();
+        f.add(s);
+        f.setSize(600, 600);
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        draw object =new draw();
+        object.drawing();
     }
-
-    public void paint(Graphics g) {
-        g.setColor(Color.BLACK);
-        g.drawOval(200, 200, 50, 50);
-        while (true) {
-            Random rand = new Random();
-            int X = rand.nextInt() + 1;
-            int Y = rand.nextInt() + 1;
-            int R = rand.nextInt() + 1;
-            g.setColor(Color.BLACK);
-            g.drawOval(X, Y, R, R);
-            try {
-                Thread.sleep(3 * 1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
 }
-
--- src/Draw.java
+++ src/Draw.java
@@ -1,13 +1,47 @@
-import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+import javax.swing.*;
 
-public class Draw extends JPanel{
-    public void drawing() {
+public class draw extends JPanel implements ActionListener {
+    public void drawing(){
         repaint();
     }
-    public void paintComponent (Graphics g) {
+
+    public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.setColor(Color.BLUE);
-        g.fillOval(480,480,200,200);
+        g.setColor(Color.BLACK);
+        g.drawOval(200, 200, 50, 50);
+        while (true) {
+            Random rand = new Random();
+            int X = rand.nextInt(500 - 25) + 1;
+            int Y = rand.nextInt(500 - 25) + 1;
+            int R = rand.nextInt(200 - 25) + 1;
+            g.setColor(Color.BLACK);
+            g.drawOval(X, Y, R, R);
+            g.drawOval(x, y, R, R);
+            t.start();
+            try {
+                Thread.sleep(2 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    Timer t = new Timer(7, this);
+    int x = 10, y = 10, velX = 7, velY = 7;
+    public void actionPerformed(ActionEvent e) {
+        if(x<0 || x > getWidth()) {
+            velX = -velX;
+        }
+        if(y < 0 || y > getHeight()) {
+            velY = -velY;
+        }
+        x += velX;
+        y += velY;
+        repaint();
     }
 }
+
+
